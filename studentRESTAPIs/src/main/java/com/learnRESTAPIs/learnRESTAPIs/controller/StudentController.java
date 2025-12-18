f6ed4a16b74531baab06da738ce7390d40749e15
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-12-19 00:16:36 +0530

    feat: Initialize student REST API project with Spring Boot
    - Added .gitignore to exclude build files and IDE configurations.
    - Created main application class `LearnRestapIsApplication`.
    - Implemented `StudentController` for handling student-related requests.
    - Defined `StudentDto` for data transfer between layers.
    - Created `Student` entity for JPA persistence.
    - Established `StudentRepository` interface for database operations.
    - Developed `StudentService` interface and its implementation `StudentServiceImpl`.
    - Configured PostgreSQL datasource in `application.properties`.
    - Added necessary dependencies in `pom.xml` for Spring Boot, JPA, and PostgreSQL.
    - Included test class `LearnRestapIsApplicationTests` for context loading verification.

--- studentRESTAPIs/src/main/java/com/learnRESTAPIs/learnRESTAPIs/controller/StudentController.java
+++ studentRESTAPIs/src/main/java/com/learnRESTAPIs/learnRESTAPIs/controller/StudentController.java
@@ -4,7 +4,6 @@ import com.learnRESTAPIs.learnRESTAPIs.dto.StudentDto;
 import com.learnRESTAPIs.learnRESTAPIs.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +20,7 @@ public class StudentController {
     }
 
     @GetMapping("/student/{id}")
-    public StudentDto getStudentById(@PathVariable Long id){
+    public StudentDto getStudentById(){
         return new StudentDto(4L,"Rakesh","rakesh@gmail.com");
     }
 }
---
+++ studentRESTAPIs/src/test/java/com/learnRESTAPIs/learnRESTAPIs/LearnRestapIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnRESTAPIs.learnRESTAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnRestapIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
